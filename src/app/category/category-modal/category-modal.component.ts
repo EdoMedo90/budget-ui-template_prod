9139faf51a4911984acfcd7e334ac4af63b3a568
Edo <edomedo31@gmail.com>
2023-03-07 21:56:30 +0100

    bla2

--- src/app/category/category-modal/category-modal.component.ts
+++ src/app/category/category-modal/category-modal.component.ts
@@ -2,19 +2,47 @@ import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ActionSheetService } from '../../shared/service/action-sheet.service';
 import { filter, from } from 'rxjs';
+import { CategoryService } from '../category.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastService} from '../../shared/service/toast.service';
+import { Category} from '../../shared/domain';
+import { FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-category-modal',
   templateUrl: './category-modal.component.html',
 })
 export class CategoryModalComponent {
-  constructor(private readonly actionSheetService: ActionSheetService, private readonly modalCtrl: ModalController) {}
+
+  constructor(
+    private readonly actionSheetService: ActionSheetService,
+    private readonly categoryService: CategoryService,
+    private readonly formBuilder: FormBuilder,
+    private readonly modalCtrl: ModalController,
+    private readonly toastService: ToastService
+  ) {
+    this.categoryForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(40)]]
+    })
+  }
 
   cancel(): void {
     this.modalCtrl.dismiss(null, 'cancel');
   }
 
   save(): void {
+    this.submitting = true;
+    this.categoryService.upsertCategory(this.categoryForm.value).subscribe({
+      next: () => {
+        this.toastService.displaySuccessToast('Category saved');
+        this.modalCtrl.dismiss(null, 'refresh');
+        this.submitting = false;
+      },
+      error: (error) => {
+        this.toastService.displayErrorToast('Could not save category', error);
+        this.submitting = false;
+      }
+    })
     this.modalCtrl.dismiss(null, 'save');
   }
 
@@ -27,4 +55,7 @@ export class CategoryModalComponent {
         },
       });
   }
+  category: Category = {} as Category;
+  readonly categoryForm: FormGroup;
+  submitting = false;
 }
